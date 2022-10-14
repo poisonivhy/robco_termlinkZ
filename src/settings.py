4fb9e4fbd35d219172ffa3f9fd4b28d102021ee2
itstealthki <83789082+itstealthki@users.noreply.github.com>
2022-10-13 21:56:27 -0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -5,5 +5,3 @@ BASE_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)))
 CREDENTIAL_STORAGE = os.path.join(BASE_DIR, "yummy.json")
 
 DATA_STORAGE = os.path.join(BASE_DIR, "data.json")
-
-
